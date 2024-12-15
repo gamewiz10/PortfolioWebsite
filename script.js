1f0298d7ef7d8223c399f2a35bc50e48d0d0b50d
gamewiz10 <joseph.bell.20@cnu.edu>
2024-12-14 20:43:19 -0500

    added github links and filled out about section

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch('about.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('about-section').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading about section:', error));
