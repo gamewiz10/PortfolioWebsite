a1c39943fee0acfd0e4babf189aa63f8abbc6301
gamewiz10 <joseph.bell.20@cnu.edu>
2024-12-20 21:04:32 -0500

    prototype complete

---
+++ download.js
@@ -0,0 +1,14 @@
+function downloadResume(filename, filepath) {
+    let element = document.createElement('a');
+    element.setAttribute('href', filepath);
+    element.setAttribute('download', filename);
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+document.getElementById("downloadResume").addEventListener("click", function () {
+                let filename = "Joseph_A_Bell_Resume_Main_rf.docx";
+                let filepath = "Joseph_A_Bell_Resume_Main_rf.docx";
+                downloadResume(filename, filepath);
+            });
